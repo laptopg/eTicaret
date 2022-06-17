90e193ce658d9d666686becf2d756f4aa21f0922
azaderdogan <nihatazad56@gmail.com>
2022-06-17 14:26:24 +0300

    master

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//deneme
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
